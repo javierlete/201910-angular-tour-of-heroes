c0f119fdd129104c3d4f3b3bde93856054487724
Javier Lete <javier@eskura21.com>
2019-10-09 20:53:28 +0200

    Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +10,40 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
+
+  getHeroes() {
+    console.log('Antes');
+    this.heroService.getHeroes().subscribe(
+      // 1 //this.cogerLlamada.bind(this)
+
+      // 2 //function(heroes: Hero[]) {
+      // 2 //  console.log('cogerLlamada2', heroes);
+      // 2 //  this.heroes = heroes;
+      // 2 //}.bind(this)
+
+      // original // heroes => this.heroes = heroes
+
+      (heroesRecibidos: Hero[]) => {
+        console.log('cogerLlamada Definitivo', heroesRecibidos);
+        this.heroes = heroesRecibidos;
+      }
+    );
+    console.log('Después');
+  }
+
+  // 1 // cogerLlamada(heroes: Hero[]) {
+  // 1 //   console.log('cogerLlamada', heroes);
+  // 1 //   this.heroes = heroes;
+  // 1 // }
 
   onSelect(hero: Hero) {
     console.log('onSelect', hero);
